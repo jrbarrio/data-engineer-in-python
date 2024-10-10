d7e5693a669da3656ebbea786c80b3c1acc77675
Jorge Roldan <jorge.roldan@gmail.com>
2024-10-10 21:23:06 +0200

    Variables and data types

---
+++ 02-introduction-to-python-for-developers/src/3.2-checking-data-types.py
@@ -0,0 +1,12 @@
+current_quarter = 4
+
+# Print the data type of current_quarter
+print(type(current_quarter))
+
+# Print the data type of 9.75
+print(type(9.75))
+
+revenue_on_target = True
+
+# Print the data type of revenue_on_target
+print(type(revenue_on_target))
