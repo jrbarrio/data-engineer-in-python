fac6995ad48de075f3eae6f55dd8846253c9283a
Jorge Roldan <jorge.roldan@gmail.com>
2024-10-10 21:48:32 +0200

    Lists

---
+++ 02-introduction-to-python-for-developers/src/2-working-with-data-types/2.2-subsetting-lists.py
@@ -0,0 +1,11 @@
+# Create the playlist variable
+playlist = [1, 'Blinding Lights', 'The Weeknd', 2, 'One Dance', 'Drake', 3, 'Uptown Funk', 'Mark Ronson', 4, 'Closer', 'The Chainsmokers', 5, 'One Kiss', 'Calvin Harris', 6, 'Mr. Brightside', 'The Killers']
+
+# Find the second song
+print(playlist[4])
+
+# Get the last song's artist
+print(playlist[-1])
+
+# Print all songs in the playlist
+print(playlist[1::3])
