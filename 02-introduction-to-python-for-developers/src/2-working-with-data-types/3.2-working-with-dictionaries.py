36ddeff455ba376137180412bd859ec15adcc3fa
Jorge Roldan <jorge.roldan@gmail.com>
2024-10-10 22:07:58 +0200

    Dictionaries

---
+++ 02-introduction-to-python-for-developers/src/2-working-with-data-types/3.2-working-with-dictionaries.py
@@ -0,0 +1,10 @@
+playlist = {'The Weeknd': 'Blinding Lights', 'Drake': 'One Dance', 'Mark Ronson': 'Uptown Funk', 'The Chainsmokers': 'Closer', 'Calvin Harris': 'One Kiss', 'The Killers': 'Mr. Brightside', 'Oasis': 'Wonderwall', 'Rihanna': 'We Found Love', 'Coldplay': 'Paradise'}
+
+# Print the song by Coldplay
+print(playlist["Coldplay"])
+
+# Add a new song
+playlist["Usher"] = "Yeah!"
+
+# Print the songs in the playlist
+print(playlist.values())
