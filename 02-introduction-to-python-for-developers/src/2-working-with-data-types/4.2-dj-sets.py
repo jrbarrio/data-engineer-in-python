db59ca94169a065fdda8d6c4103de7448fbc947a
Jorge Roldan <jorge.roldan@gmail.com>
2024-10-10 22:17:19 +0200

    Sets and tuples

---
+++ 02-introduction-to-python-for-developers/src/2-working-with-data-types/4.2-dj-sets.py
@@ -0,0 +1,10 @@
+hip_hop = ["Drake", "JAY-Z", "50 Cent", "Drake"]
+
+# Create a set
+indie_set = {"Kings of Leon", "MGMT", "Stereophonics"}
+
+# Convert hip_hop to a set
+hip_hop_set = set(hip_hop)
+
+# Print the indie and hip_hop sets
+print(indie_set, hip_hop_set)
