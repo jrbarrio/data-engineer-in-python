22ced833540e96403b15b2a7b3d3801c40bdd2ba
Jorge Roldan <jorge.roldan@gmail.com>
2024-10-13 13:35:09 +0200

    Built-in functions

---
+++ 03-intermediate-python-for-developers/src/1-built-in-functions/1.1-get-some-assistance.py
@@ -0,0 +1,8 @@
+# Find out more about len()
+print(help(len))
+
+# Find out more about int
+print(help(int))
+
+# Find out more about lists
+print(help(list))
---
+++ 03-intermediate-python-for-developers/src/1-built-in-functions/1.3-performing-calculations.py
@@ -0,0 +1,13 @@
+course_completions = [97, 83, 121, 205, 56, 174, 92, 117, 164]
+
+# Print the total number of course completions
+print(sum(course_completions))
+
+# Print the largest number of completions
+print(max(course_completions))
+
+# Print the average number of completions
+print(sum(course_completions) / len(course_completions))
+
+# Print the average number of completions, rounded to one decimal places
+print(round(sum(course_completions) / len(course_completions), 1))
