659434c35aa4f83930686122890c897df6778245
Jorge Roldan <jorge.roldan@gmail.com>
2024-11-21 18:47:20 +0100

    Adding functionality to classes

--- 11-software-engineering-principles-in-python/src/3-utilizing-classes/text_analyzer/document.py
+++ 11-software-engineering-principles-in-python/src/3-utilizing-classes/text_analyzer/document.py
@@ -1,11 +1,17 @@
-# Define Document class
+from .token_utils import tokenize
+from collections import Counter
+
 class Document:
-  """A class for text analysis
-  
-  :param text: string of text to be analyzed
-  :ivar text: string of text to be analyzed; set by `text` parameter
-  """
-  # Method to create a new instance of Document
   def __init__(self, text):
-    # Store text parameter to the text attribute
     self.text = text
+    # pre tokenize the document with non-public tokenize method
+    self.tokens = self._tokenize()
+    # pre tokenize the document with non-public count_words
+    self.word_counts = self._count_words()
+
+  def _tokenize(self):
+    return tokenize(self.text)
+	
+  # non-public method to tally document's word counts with Counter
+  def _count_words(self):
+    return Counter(self.tokens)
